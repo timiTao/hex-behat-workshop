8e02cdfa9c978e1eb129825090c0aa57c2f79750
TimiTao <kunicki.tomasz@gmail.com>
2017-11-01 18:08:59 +0100

    Basics 3. result diffrent context

---
+++ features/bootstrap/System/PlayerFeatureContext.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * @author Tomasz Kunicki <kunicki.tomasz@gmail.com>
+ */
+
+namespace System;
+
+use Behat\Behat\Context\Context;
+use Helpers\Domain\Repository\PlayerRepository;
+use PHPUnit\Framework\Assert;
+use Workshops\Domain\Player;
+use Workshops\Domain\UseCase\GetPlayer\GetPlayerQuery;
+use Workshops\Domain\UseCase\GetPlayer\GetPlayerUseCase;
+use Workshops\Domain\UseCase\GetPlayer\Response\Player as GetPlayerResponse;
+use Workshops\Domain\UseCase\ThrowBall\ThrowBallCommand;
+use Workshops\Domain\UseCase\ThrowBall\ThrowBallUseCase;
+
+class PlayerFeatureContext implements Context
+{
+    /**
+     * @var PlayerRepository
+     */
+    private $playerRepository;
+
+    /**
+     * @var GetPlayerUseCase
+     */
+    private $getPlayerUseCase;
+
+    /**
+     * @var GetPlayerResponse
+     */
+    private $getPlayerResponse;
+
+    /**
+     * @var ThrowBallUseCase
+     */
+    private $throwBallUserCase;
+
+    /**
+     */
+    public function __construct()
+    {
+        $this->playerRepository = new PlayerRepository();
+        $this->getPlayerUseCase = new GetPlayerUseCase($this->playerRepository);
+        $this->throwBallUserCase = new ThrowBallUseCase($this->playerRepository);
+    }
+
+    /**
+     * @Given player :name
+     */
+    public function player(string $name)
+    {
+        $this->playerRepository->add(new Player($name));
+    }
+
+    /**
+     * @Then player :expectedName exist
+     */
+    public function playerExist(string $expectedName)
+    {
+        $query = new GetPlayerQuery($expectedName);
+        $this->getPlayerResponse = $this->getPlayerUseCase->handle($query);
+    }
+
+    /**
+     * @When player :name throw ball
+     */
+    public function playerThrowBall(string $name)
+    {
+        $command = new ThrowBallCommand($name);
+        $this->throwBallUserCase->handle($command);
+    }
+
+    /**
+     * @Then player :name should have :points point
+     */
+    public function playerShouldHavePoint(string $name, int $expectedPoints)
+    {
+        $query = new GetPlayerQuery($name);
+        $response = $this->getPlayerUseCase->handle($query);
+        Assert::assertEquals($expectedPoints, $response->points);
+    }
+}
